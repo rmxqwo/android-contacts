0d7d9689e19134be110e8ecc992c0c82e3439ae6
rmxqwo <158151985+rmxqwo@users.noreply.github.com>
2025-12-26 23:26:00 +0300

    pr
    project

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeUI.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeUI.java
@@ -2,9 +2,10 @@ package ru.yandex.practicum.contacts.presentation.sort;
 
 import androidx.annotation.NonNull;
 
+import ru.yandex.practicum.contacts.presentation.base.ListDiffInterface;
 import ru.yandex.practicum.contacts.presentation.sort.model.SortType;
 
-public class SortTypeUI {
+public class SortTypeUI implements ListDiffInterface<SortTypeUI> {
 
     private final SortType sortType;
     private final boolean selected;
@@ -14,6 +15,10 @@ public class SortTypeUI {
         this.selected = selected;
     }
 
+    @Override
+    public boolean theSameAs(SortTypeUI item) {
+        return this.getSortType() == item.getSortType();
+    }
     public SortType getSortType() {
         return sortType;
     }
